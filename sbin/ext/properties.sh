a9587e7c07b735cfa7b071aab3b7744e45291b0e
Talustus <talustus@dream-irc.de>
2012-07-07 01:43:37 +0200

    Version 2.5

--- sbin/ext/properties.sh
+++ sbin/ext/properties.sh
@@ -4,7 +4,7 @@ mkdir -p /customkernel/property
 echo true >> /customkernel/property/customkernel.cf-root 
 echo true >> /customkernel/property/customkernel.base.cf-root 
 echo DreamKernel >> /customkernel/property/customkernel.name 
-echo "DreamKernel 2.4" >> /customkernel/property/customkernel.namedisplay 
+echo "DreamKernel 2.5" >> /customkernel/property/customkernel.namedisplay 
 echo 104 >> /customkernel/property/customkernel.version.number 
 echo 5.2 >> /customkernel/property/customkernel.version.name 
 echo true >> /customkernel/property/customkernel.bootani.zip 
